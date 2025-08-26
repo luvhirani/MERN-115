28a198090c581073029a0ec203d8b8c7174442ba
luvhirani <luvhirani0@gmail.com>
2025-08-26 09:33:03 +0530

    node+mysql

--- Express/controller/product.js
+++ Express/controller/product.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const pool = require("../db/db")
 
 //localhost:3012/product/createProduct
@@ -5,7 +6,6 @@ const createProduct = async (req, res) => {
     // console.log("request Object:",req)
 
     let {pid,name, price, category} = req.body;
-
     console.log(pid,name,price,category)
     try {
         const response = await pool.query("INSERT INTO products VALUES (?,?,?,?)",[pid,name,price,category])
@@ -15,17 +15,54 @@ const createProduct = async (req, res) => {
     }
 }
 
-const getAllProducts = (req,res) =>{
-
-    res.send("list of all the products")
+const getOneProduct = async (req,res) => { //localhost:3012/product/getOneProduct/2
+    try {
+        // console.log(req.params.id)
+        const pid = req.params.id; 
+        const response = await pool.query("SELECT * FROM products WHERE pid=?",[pid]);
+        res.send(response[0])
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-const updateProduct = (req,res) =>{
+const getAllProducts = async (req,res) =>{ //localhost:3012/product/getAllProducts
 
+    try {
+        const response = await pool.query("SELECT * FROM products");
+        // console.log(response[0])
+        res.send(response[0])
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
 }
 
-const deleteProduct = (req,res) =>{
-
+const updateProduct = async (req,res) =>{
+    let pId = req.params.id;
+    let {name, price, category} = req.body;
+    // console.log(pId,name,price)
+    try{
+        let response = await pool.query("UPDATE products SET name=?,price=?, category=? WHERE pId=?",[name,price,category,pId])
+        console.log(response)
+        res.send({message:"Product updated"});
+    }
+    catch(error){
+        console.log(error)
+    }
 }
 
-module.exports = {getAllProducts,updateProduct,deleteProduct,createProduct}
+const deleteProduct = async (req,res) =>{
+    let pid = req.params.id;
+    try {
+        const response = await pool.query("DELETE FROM products WHERE pId=?",[pid])
+        res.send({message:"Product deleted", response});
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {getAllProducts,updateProduct,deleteProduct,createProduct,getOneProduct}
+
+
+
--- Express/routes/product.js
+++ Express/routes/product.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {createProduct,deleteProduct,updateProduct,getAllProducts} = require("../controller/product")
+const {createProduct,deleteProduct,updateProduct,getAllProducts, getOneProduct} = require("../controller/product")
 
 router.get("/getAllProducts", getAllProducts)
+router.get("/getOneProduct/:id", getOneProduct)
 router.post("/createProduct",createProduct)
-router.put("/updateProduct", updateProduct)
-router.delete("/deleteProduct", deleteProduct)
+router.put("/updateProduct/:id", updateProduct)
+router.delete("/deleteProduct/:id", deleteProduct)
 
 module.exports = router
